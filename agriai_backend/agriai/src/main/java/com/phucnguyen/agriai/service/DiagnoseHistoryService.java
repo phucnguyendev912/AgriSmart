093734e197aa60e053c3d4bee8226471debb712b
djwin2609 <ID+djwin2609@users.noreply.github.com>
2026-05-12 15:27:57 +0700

    update diagnose

--- agriai_backend/agriai/src/main/java/com/phucnguyen/agriai/service/DiagnoseHistoryService.java
+++ agriai_backend/agriai/src/main/java/com/phucnguyen/agriai/service/DiagnoseHistoryService.java
@@ -1,14 +1,10 @@
 package com.phucnguyen.agriai.service;
-
 import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.phucnguyen.agriai.dto.DiagnosisDetailSnapshotDTO;
 import com.phucnguyen.agriai.entity.DiagnoseHistory;
